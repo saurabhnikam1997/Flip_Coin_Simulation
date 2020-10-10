49214df7bd4d7544d4d4890d2a6919a8019eb649
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-10 12:46:54 +0530

    Flipped coin for multiple times and show winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,9 +2,26 @@
 
 echo "Welcome To Flip Coin Simulation Problem"
 
-if [[ $((RANDOM%2)) -eq 1 ]]
-then
-	echo Heads
-else
-	echo Tails
-fi
+headcount=0
+tailcount=0
+
+while [[ $headcount -ne 10 && $tailcount -ne 11 ]]
+do
+	coinoutput=$((RANDOM % 2))
+	if [[ $coinoutput -eq 1 ]]
+	then
+		((headcount++))
+	else
+		((tailcount++))
+	fi
+
+	if [[ $headcount -eq 10 ]]
+	then
+		echo "Head count 10..Head Is Won"
+
+	elif [[ $tailcount -eq 10 ]]
+	then
+		echo "Tail count 10.. Tail Is Won"
+		exit
+	fi
+done
